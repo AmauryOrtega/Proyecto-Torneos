4aae60216c77e42cf6c0b236474401c3606f2dce
Amaury Ortega <amauryocortega@gmail.com>
2017-12-03 14:22:13 -0500

    Proyecto que simula listo

---
+++ App-simulando/src/modelo/Jugador.java
@@ -0,0 +1,74 @@
+package modelo;
+
+public class Jugador {
+
+    private final String nombre_completo;
+    private final String cedula;
+    private final String cinturon;
+    private final String sexo;
+    private final float peso;
+    private final int edad;
+    private final float estatura;
+
+    public Jugador(String nombre_completo, String cedula, String cinturon, String sexo, float peso, int edad, float estatura) {
+        this.nombre_completo = nombre_completo;
+        this.cedula = cedula;
+        this.cinturon = cinturon;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.edad = edad;
+        this.estatura = estatura;
+    }
+
+    public Jugador(String nombre_completo, String cedula) {
+        this.nombre_completo = nombre_completo;
+        this.cedula = cedula;
+        this.cinturon = "";
+        this.sexo = "";
+        this.peso = 0;
+        this.edad = 0;
+        this.estatura = 0;
+    }
+
+    public String getNombre_completo() {
+        return nombre_completo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getCinturon() {
+        return cinturon;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public boolean igual(Jugador jugador) {
+        return this.nombre_completo.equals(jugador.nombre_completo) && this.cedula.equals(jugador.cedula);
+    }
+
+    @Override
+    public String toString() {
+        return nombre_completo + ", CC." + cedula + ", cinturon=" + cinturon + ", sexo=" + sexo + ", peso=" + peso + ", edad=" + edad + ", estatura=" + estatura;
+    }
+    
+    public String toStringCorto() {
+        return nombre_completo + " CC." + cedula;
+    }
+
+}
---
+++ App-simulando/src/modelo/Torneo.java
@@ -0,0 +1,205 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Torneo {
+
+    private final String cinturon;
+    private final String sexo;
+    private final String edad;
+    private final String deporte;
+    private final String peso;
+
+    private int ronda;
+
+    private ArrayList<Jugador> jugadores;
+    private final ArrayList<Jugador> jugadores_bak;
+
+    private ArrayList<Jugador> extras;
+    private final ArrayList<Jugador> extras_bak;
+
+    private ArrayList<Partido> partidos;
+
+    private Torneo torneo_terceristas_1;
+    private Torneo torneo_terceristas_2;
+
+    public Torneo(ArrayList<Jugador> jugadores, String cinturon, String sexo, String edad, String deporte, String peso) {
+        this.jugadores = jugadores;
+        this.jugadores_bak = (ArrayList<Jugador>) jugadores.clone();
+        this.partidos = new ArrayList<>();
+        this.extras = new ArrayList<>();
+        // Escogiendo extras de Extras
+        Random r = new Random();
+        int n_equipos = this.jugadores.size();
+        for (int i = 0; i < this.numero_byes(n_equipos); i++) {
+            this.extras.add(this.jugadores.remove(r.nextInt(this.jugadores.size())));
+        }
+        this.extras_bak = (ArrayList<Jugador>) this.extras.clone();
+        // Primera ronda de partidos
+        this.ronda = 0;
+        int n_partidos = this.jugadores.size() / 2;
+        for (int i = 0; i < n_partidos; i++) {
+            this.partidos.add(new Partido(
+                    this.jugadores.remove(r.nextInt(this.jugadores.size())),
+                    this.jugadores.remove(r.nextInt(this.jugadores.size())),
+                    this.ronda)
+            );
+        }
+        this.cinturon = cinturon;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.deporte = deporte;
+        this.peso = peso;
+    }
+
+    public int getRonda() {
+        return ronda;
+    }
+
+    public String getCinturon() {
+        return cinturon;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public ArrayList<Jugador> getJugadores_bak() {
+        return jugadores_bak;
+    }
+
+    public ArrayList<Jugador> getExtras_bak() {
+        return extras_bak;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public ArrayList<Partido> getPartido(int ronda) {
+        ArrayList<Partido> lista = new ArrayList<>();
+        for (Partido partido : this.partidos) {
+            if (partido.getRonda() == ronda) {
+                lista.add(partido);
+            }
+        }
+        return lista;
+    }
+
+    public ArrayList<Partido> getPartidosActuales() {
+//        this.partidos.stream().filter((partido) -> (partido.getRonda() == this.ronda)).forEachOrdered(lista::add);
+        ArrayList<Partido> lista = new ArrayList<>();
+        for (Partido partido : this.partidos) {
+            if (partido.getRonda() == this.ronda) {
+                lista.add(partido);
+            }
+        }
+        return lista;
+    }
+
+    public Torneo getTorneo_terceristas_1() {
+        return torneo_terceristas_1;
+    }
+
+    public Jugador getGanador() {
+        if (this.getPartidosActuales().size() == 1) {
+            return this.getPartidosActuales().get(0).getGanador();
+        } else {
+            return null;
+        }
+    }
+
+    public ArrayList<Jugador> getTerceristas2() {
+        ArrayList<Jugador> lista = new ArrayList<>();
+        Jugador segundo = this.getPartidosActuales().get(0).getPerdedor();
+        for (Partido partido : this.getPartidos()) {
+            if (partido.getGanador().igual(segundo)) {
+                lista.add(partido.getPerdedor());
+            }
+        }
+        return lista;
+    }
+
+    public void siguienteRonda() {
+        ArrayList<Jugador> ganadores = new ArrayList<>();
+        // Llenando ganadores
+        this.getPartidosActuales().forEach((partido) -> {
+            ganadores.add(partido.getGanador());
+        });
+        if (this.ronda == 0) {
+            // Solo si hay numero_byes, los agrega
+            this.extras.forEach((bye) -> {
+                ganadores.add(bye);
+            });
+        }
+        // Creando partidos con ganadores de la ronda anterior
+        if (ganadores.size() != 1) {
+            this.ronda++;
+            int n_partidos = ganadores.size() / 2;
+            Random r = new Random();
+            for (int i = 0; i < n_partidos; i++) {
+                this.partidos.add(new Partido(
+                        ganadores.remove(r.nextInt(ganadores.size())),
+                        ganadores.remove(r.nextInt(ganadores.size())),
+                        this.ronda)
+                );
+            }
+        }
+    }
+
+    public void iniciarMiniTorneo1() {
+        if (this.getGanador() != null) {
+            ArrayList<Jugador> terceristas = this.getTerceristas1();
+            this.torneo_terceristas_1 = new Torneo(terceristas, this.cinturon, this.sexo, this.edad, this.deporte, this.peso);
+        }
+    }
+
+    public ArrayList<Jugador> getTerceristas1() {
+        ArrayList<Jugador> lista = new ArrayList<>();
+        for (Partido partido : this.getPartidos()) {
+            if (partido.getGanador().igual(this.getGanador())) {
+                lista.add(partido.getPerdedor());
+            }
+        }
+        // No puede volver a jugar el que quedo de segundo
+        lista.remove(this.getPartidosActuales().get(0).getPerdedor());
+        return lista;
+    }
+
+    public int numero_byes(int n_equipos) {
+        if ((Math.log(n_equipos) / Math.log(2)) == (int) (Math.log(n_equipos) / Math.log(2))) {
+            return 0;
+        } else {
+            int i = 1;
+            while (Math.pow(2, i) <= n_equipos) {
+                i++;
+            }
+            return (int) Math.pow(2, i) - n_equipos;
+        }
+    }
+
+    public void iniciarMiniTorneo2() {
+        if (this.getGanador() != null) {
+            ArrayList<Jugador> terceristas = this.getTerceristas2();
+            this.torneo_terceristas_2 = new Torneo(terceristas, this.cinturon, this.sexo, this.edad, this.deporte, this.peso);
+        }
+    }
+
+    public Torneo getTorneo_terceristas_2() {
+        return torneo_terceristas_2;
+    }
+
+}
